6f339f0d23c32f28a340d958f6b75ab837fb96e7
trustpizza <maxelolsson@gmail.com>
2022-12-01 18:56:20 -0600

    Add edit and update methods to page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :new]
+  resources :users, only: [:create, :new, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
